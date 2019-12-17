65e7eb3a4961665d3ae325d01658302c23b1a37b
Jessica Wolff <jleahwolff@gmail.com>
2019-12-17 13:21:15 -0600

    Finished the App.js page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-
+import { Route, Router, Switch } from 'react-router-dom';
+import Movie from "./Movies/Movie";
+import MovieList from "./Movies/MovieList";
 import SavedList from './Movies/SavedList';
 
 const App = () => {
@@ -12,7 +14,14 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Switch>
+      <Route exact path="/">
+        <MovieList></MovieList>
+      </Route>
+      <Route path="/movies/:movieid">
+        <Movie></Movie>
+      </Route>
+      </Switch>
     </div>
   );
 };
